aa533227db09993024ec80479611b0d6b041644b
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-12-12 10:24:09 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '../lib/AuthProvider';
+ 
+
+// height: 900px; width: 50px; position: absolute; for the CSS
+
+class Navbar extends Component {
+  render() {
+    const { user, logout, isLoggedin } = this.props;
+    return (
+      <div
+        style={{ borderRadius: '5px', padding: '20px', background: '#686de0' }}>
+        {isLoggedin ? (
+          <div>
+            <p>username: {user.username}</p>
+            <button onClick={logout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">
+              {' '}
+              <button>Login</button>{' '}
+            </Link>
+            <br />
+            <Link to="/signup">
+              {' '}
+              <button>Signup</button>{' '}
+            </Link>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default withAuth(Navbar);
---
+++ src/pages/Private.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Private extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Private Route</h1>
+      </div>
+    );
+  }
+}
+
+export default Private;
+ 
