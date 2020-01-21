7b61ebacf1845722060f9d4a007a55b16306db9f
Fred011 <54903136+Fred011@users.noreply.github.com>
2020-01-21 15:49:46 +0100

    last update for responsivness

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css'
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Default from './pages/Default';
-import Profile from './pages/Profile';
-import EditProfile from './components/EditProfile';
+import "./App.css";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Default from "./pages/Default";
+import Profile from "./pages/Profile";
+import EditProfile from "./components/EditProfile";
 
-import UserTopicDetails from './components/UserTopicDetails'
-import Search from './components/Search';
-import MyTopics from './pages/MyTopics';
-import MyComments from './pages/MyComments';
-import TopicDetails from './components/TopicDetails.js'
-import Favorites from './pages/Favorites'
+import UserTopicDetails from "./components/UserTopicDetails";
+import Search from "./components/Search";
+import MyTopics from "./pages/MyTopics";
+import MyComments from "./pages/MyComments";
+import TopicDetails from "./components/TopicDetails.js";
+import Favorites from "./pages/Favorites";
 
-
-import AnonRoute from './components/AnonRoute';
-import PrivateRoute from './components/PrivateRoute';
-import NewTopic from './pages/NewTopic';
-import topicService from './lib/topic-service';
-
-
+import AnonRoute from "./components/AnonRoute";
+import PrivateRoute from "./components/PrivateRoute";
+import NewTopic from "./pages/NewTopic";
+import topicService from "./lib/topic-service";
 
 class App extends Component {
-
   state = {
     topicsArr: [],
     showNav: false,
-    search: ''
-  }
-
-  componentDidMount() {
-    topicService
-      .getAllTopics()
-        .then( (data) => {
-          this.setState({ topicsArr: data });
-        })
-        .catch( (err) => console.log(err));
-  }
+    search: ""
+  };
 
   // updateSearch = (e) => {
   //   const query = e.target.value;
@@ -47,10 +34,10 @@ class App extends Component {
   //   const filtered = this.state.topicsArr.filter((topic) => {
   //     return <div> topic.title.toLowerCase().includes(query.toLowerCase()) </div>
   //   })
-    
+
   //   this.setState({search: query})
   // }
-  
+
   render() {
     return (
       <div className="App">
@@ -58,7 +45,6 @@ class App extends Component {
           searching={this.state.search} 
           //theSearch={this.updateSearch}
         /> */}
-        {/* <h1>Basic React Authentication</h1> */}
         <Switch>
           <AnonRoute exact path="/signup" component={Signup} />
           <AnonRoute exact path="/login" component={Login} />
@@ -67,14 +53,16 @@ class App extends Component {
           <PrivateRoute exact path="/addtopic" component={NewTopic} />
           <PrivateRoute exact path="/topic/:id" component={TopicDetails} />
           <PrivateRoute exact path="/mytopics" component={MyTopics} />
-          <PrivateRoute exact path="/mytopics/:id" component={UserTopicDetails} />
+          <PrivateRoute
+            exact
+            path="/mytopics/:id"
+            component={UserTopicDetails}
+          />
           <PrivateRoute exact path="/mycomments" component={MyComments} />
           <PrivateRoute exact path="/favorites" component={Favorites} />
           <PrivateRoute exact path="/" component={Home} />
-          <Route component={Default}/>
-
+          <Route component={Default} />
         </Switch>
-
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { withAuth } from '../lib/AuthProvider';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { withAuth } from "../lib/AuthProvider";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
-  state = { username: '', password: '' };
+  state = { username: "", password: "" };
 
   handleFormSubmit = event => {
     event.preventDefault();
@@ -20,15 +20,14 @@ class Login extends Component {
     const { username, password } = this.state;
 
     return (
-      <div className='login-container'>
+      <div className="login-container">
         <div className="all-login">
-          <form className='log' onSubmit={this.handleFormSubmit}>
-
+          <form className="log" onSubmit={this.handleFormSubmit}>
             <input
               type="text"
               name="username"
               value={username}
-              placeholder='username'
+              placeholder="username"
               onChange={this.handleChange}
             />
 
@@ -36,13 +35,18 @@ class Login extends Component {
               type="password"
               name="password"
               value={password}
-              placeholder='password'
+              placeholder="password"
               onChange={this.handleChange}
             />
 
-            <input className='submit-btn' type="submit" value="Login" />
+            <input className="submit-btn" type="submit" value="Login" />
 
-            <p className='log-sentence'>Don't have an account? <Link to="/signup"><span className='span-account'>Sign Up</span> </Link></p>
+            <p className="log-sentence">
+              Don't have an account?{" "}
+              <Link to="/signup">
+                <span className="span-account">Sign Up</span>{" "}
+              </Link>
+            </p>
           </form>
         </div>
       </div>
